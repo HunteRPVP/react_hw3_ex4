a72783f94816555e1c4e90ba8998e71b8ea0c8bc
HunteRPVP <55155885+HunteRPVP@users.noreply.github.com>
2021-05-09 16:00:02 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+
+  const data = [
+    {
+      title: "JSX",
+      desc: "JSX — расширение языка JavaScript. JSX - это XML-подобное расширение синтаксиса для ECMAScript без какой-либо определенной семантики."
+    },
+    {
+      title: "Компоненты",
+      desc: "Компоненты позволяют разбить интерфейс на независимые части, про которые легко думать в отдельности.",
+    },
+    {
+      title: "Пропсы",
+      desc: "Props представляет коллекцию значений, которые ассоциированы с компонентом.",
+    }
+  ]
+
+  const newsCard = ({ title, desc }) => {
+    return (
+      <div style={{
+        boxSizing: 'border-box',
+        padding: '10px',
+        border: '1px solid black',
+        width: '30%',
+        borderRadius: '10px'
+      }}>
+        <h2>{title}</h2>
+        <p>{desc}</p>
+      </div>
+    );
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img
+        style={{
+          maxWidth: "100%",
+        }}
+        src="https://habrastorage.org/files/3e5/3da/c0f/3e53dac0f1ad421d84310b552058621f.jpg"
+        alt="React"
+      />
+      <div
+      style={{
+        display: 'flex',
+        justifyContent: 'space-between'
+      }}
+      >
+        {data.map(props => newsCard(props))}
+      </div>
     </div>
   );
 }
